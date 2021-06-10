3dff2284d291d506a4bb87223929e5e28bc1206e
Max <ivannikov.maxim1@gmail.com>
2021-06-10 20:36:56 +0200

    rest and parametres

--- js/script.js
+++ js/script.js
@@ -156,12 +156,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Используем классы для карточек
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAH();
@@ -173,8 +174,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            
             element.innerHTML = `
-                <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
@@ -183,13 +191,9 @@ window.addEventListener('DOMContentLoaded', () => {
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                </div>   
             `;
             this.parent.append(element);
-
-        }
-
-        
+        }        
     } 
 
     new MenuCard(
@@ -199,6 +203,7 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
         '.menu .container'
+        
     ).render();
 
     new MenuCard(
@@ -218,6 +223,4 @@ window.addEventListener('DOMContentLoaded', () => {
         21,
         '.menu .container'
     ).render();
-
-
 });
