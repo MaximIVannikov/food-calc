63ec477cde82d68da1a3d92e020068b228602e03
Max <ivannikov.maxim1@gmail.com>
2021-06-19 13:37:08 +0200

    send data through formData

--- js/script.js
+++ js/script.js
@@ -251,32 +251,36 @@ window.addEventListener('DOMContentLoaded', () => {
             
             form.insertAdjacentElement('afterend', statusMessage);
 
+          
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
+            
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(key, value)  {
-                object[key] = value;
-            });
-            const json = JSON.stringify(object);
+            //const object = {};
+            //formData.forEach(function(key, value)  {
+            //    object[key] = value;
+            //});
+            //const json = JSON.stringify(object);
 
-            request.send(json);
+            fetch('server.php', {
+                method: "POST",
+                //headers: {
+                //    'Content-type': 'application/json'
+                //},
+                body: formData
 
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                    
-                } else {
-                    showThanksModal(message.failure);
-                }
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);                
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
+
+            
 
         });
     }
@@ -302,4 +306,18 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         },4000);
     }
+
+    //fetch('https://jsonplaceholder.typicode.com/posts', {
+    //    method: 'POST',
+    //    body: JSON.stringify({name: 'Alex'}),
+    //    headers: {
+    //        'Content-type': 'application/json'
+    //    }
+
+    //})
+    //    .then(response => response.json())
+    //    .then(json => console.log(json));
+
+
 });
+
